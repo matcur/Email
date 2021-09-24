2eb068e4e20c4ad23c041a4f4e2873115797cd65
matsur <ru.far2014@gmail.com>
2021-09-24 21:52:21 +0500

    new: add example project

---
+++ src/Email.Example/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Email.Core;
+using Email.Core.Clients;
+using Email.Core.Contents;
+
+namespace Email.Example
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var message = new EmailMessage(
+                "name",
+                "******",
+                new HtmlContent("<strong>Hi!!!</strong>")
+            );
+
+            await new GmailClient(
+                "name",
+                "*****", 
+                "*****"
+            ).Send(message.ToMimeMessage());
+        }
+    }
+}
