71dbae7e38177ffa26a98946b3db044dd647ff34
matsur <ru.far2014@gmail.com>
2021-09-24 21:39:25 +0500

    new: add default message implementation

---
+++ src/Email.Core/EmailMessage.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using Email.Core.Contents;
+using MimeKit;
+
+namespace Email.Core
+{
+    public class EmailMessage : IMessage
+    {
+        private readonly IEnumerable<MailboxAddress> _receivers;
+        
+        private readonly IMessageContent _content;
+
+        public EmailMessage(string toName, string toEmail, params IMessageContent[] contents)
+            : this(new MailboxAddress(toName, toEmail), contents)
+        {
+        }
+
+        public EmailMessage(MailboxAddress to, params IMessageContent[] contents)
+            : this(new List<MailboxAddress> {to}, contents)
+        {
+        }
+
+        public EmailMessage(IEnumerable<MailboxAddress> receivers, params IMessageContent[] contents)
+            : this(receivers, new MultipartContent(contents))
+        {
+        }
+
+        public EmailMessage(IEnumerable<MailboxAddress> receivers, IMessageContent content)
+        {
+            _receivers = receivers;
+            _content = content;
+        }
+        
+        public MimeEntity ToBody()
+        {
+            return _content.ToBody();
+        }
+
+        public IEnumerable<MailboxAddress> Targets()
+        {
+            return _receivers;
+        }
+
+        public MimeMessage ToMimeMessage()
+        {
+            var message = new MimeMessage
+            {
+                Body = ToBody(),
+            };
+            
+            message.To.AddRange(_receivers);
+
+            return message;
+        }
+    }
+}
