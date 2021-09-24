ecd9c68a3c68404f143c4ebd1bc0bb9f46545314
matsur <ru.far2014@gmail.com>
2021-09-24 21:39:41 +0500

    new: add gmail client implementation

---
+++ src/Email.Core/Clients/GmailClient.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using MailKit;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace Email.Core.Clients
+{
+    public class GmailClient : IEmailClient
+    {
+        private readonly string _name;
+        
+        private readonly string _email;
+
+        private readonly string _password;
+
+        private readonly MailTransport _transport;
+
+        public GmailClient(string name, string email, string password)
+            : this(name, email, password, new SmtpClient())
+        {
+
+        }
+
+        public GmailClient(string name, string email, string password, MailTransport transport)
+        {
+            _name = name;
+            _email = email;
+            _password = password;
+            _transport = transport;
+        }
+        
+        public async Task Send(MimeMessage message)
+        {
+            message.From.Add(new MailboxAddress(_name, _email));
+            message.From.Add(new MailboxAddress(_name, _email));
+            using (_transport)
+            {
+                await _transport.ConnectAsync("smtp.gmail.com", 587, false);
+                await _transport.AuthenticateAsync(_email, _password);
+
+                await _transport.SendAsync(message);
+                
+                await _transport.DisconnectAsync(true);
+            }
+        }
+    }
+}
